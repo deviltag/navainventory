321275b568095fc43cb3e31d96fc682ba9c252c8
nathaphol <nathaphol008@gmail.com>
2016-08-04 10:13:13 +0700

    เพิ่มไฟล์ hotkey & menutree
    - เพิ่มฟังชั่นก์ของการกด ปุ่มตัวเลข เพื่อเข้าใช้งานเมนู
    - แยกคำสั่ง เมนู panel ออกจากไฟล์ index.html

---
+++ platforms/android/assets/www/js/hotkey.js
@@ -0,0 +1,40 @@
+document.addEventListener("keydown", function(event) {
+        console.log(event.keyCode);
+        var page="";
+        page = $.mobile.activePage.attr('id');
+        console.log(page);
+
+            if(page=="pagetwo"){
+                switch (event.keyCode){
+                    case 49: $.mobile.changePage("#pagepr");
+                            break;
+                    case 50: $.mobile.changePage("#receive");
+                            break;
+                    case 51: $.mobile.changePage("#stock");
+                            break;
+                    case 52: $.mobile.changePage("#transfer");
+                            break;
+                    default: $.mobile.changePage("#pagetwo");
+                            break;
+                }
+            }
+
+            if(page=="transfer"){
+                switch (event.keyCode){
+                    case 49: $.mobile.changePage("#transferup");
+                            break;
+                    case 50: $.mobile.changePage("#transferdown");
+                            break;
+                    case 52: $.mobile.changePage("#damage");
+                            break;
+                    case 53: $.mobile.changePage("#isp");
+                            break;
+                    case 55: $.mobile.changePage("#rtv");
+                            break;
+                    case 56: $.mobile.changePage("#transfer_normal");
+                            break;
+                    default: $.mobile.changePage("#transfer");
+                            break;
+                }
+            }
+});
