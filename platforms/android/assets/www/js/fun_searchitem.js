e6a3ea344f7d46d42d8c8f42f3ac025ad20235cd
nathaphol <nathaphol008@gmail.com>
2016-08-18 13:04:21 +0700

    ฟังก์ชั่นของหน้า search item

---
+++ platforms/android/assets/www/js/fun_searchitem.js
@@ -0,0 +1,14 @@
+function rewh(){
+    var whdetail = "";
+    var item = "";
+    whdetail = '<font color="red">SCANBARCODE WAREHOUSE</font>';
+    item = '<font color="red">SCANBARCODE ITEM</font>';
+
+     document.getElementById("whtitle").innerHTML = whdetail;
+     document.getElementById("valwh").value = "";
+     document.getElementById("itemdtail").innerHTML = item;
+     document.getElementById("whtitle").style.textAlign = "center";
+     document.getElementById("itemdtail").style.textAlign = "center";
+
+     $.mobile.changePage("#searchitem");
+}
